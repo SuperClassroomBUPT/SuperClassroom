25418cf2ea5309956b55dd9d79dc2f2d6e033c25
userdrzh <xh-wang@163.com>
2021-04-26 03:29:11 +0800

    arduino

--- SuperClassroom_Arduino/SuperClassroom_Arduino.ino
+++ SuperClassroom_Arduino/SuperClassroom_Arduino.ino
@@ -1,13 +1,12 @@
 #include <Adafruit_NeoPixel.h>
 #include <SoftwareSerial.h>
-SoftwareSerial blt(12, 13);
-//12RXD接收13TXD发送
+SoftwareSerial blt(0, 1);
 
 unsigned long oldtime = 0;
 const int vib_i =  11, vib_o = 10;//震动
 #define snd_ia  A1//声音
 const int snd_id = 4, snd_o  = 8;
-const int l  = 7, m  = 6, r  = 5;//识别
+const int l  = 7, m  = 6, r  = 5;
 #define lit_i  A0//环境光
 const int lit_o  = 3;
 
@@ -28,7 +27,8 @@ void setup() {
     stripl.setPixelColor(i, stripl.Color(255, 255, 255));
     stripm.setPixelColor(i, stripm.Color(255, 255, 255));
     stripr.setPixelColor(i, stripr.Color(255, 255, 255));
-  } stripl.show(); stripm.show(); stripr.show();//三条灯全亮
+  }
+  stripl.show(); stripm.show(); stripr.show();//三条灯全亮
 
 }
 
@@ -36,41 +36,61 @@ void loop() {
   //震动
   float _vibin = digitalRead(vib_i);
   //Serial.println(_vibin);
-  if (!_vibin) digitalWrite(vib_o, HIGH);
+  if (!_vibin) {
+   // digitalWrite(vib_o, HIGH);
+    stripm.setBrightness(255);
+    stripm.show();
+  }
 
   if (millis() - oldtime > 5000) {
     oldtime = millis(); //更新时间点
     digitalWrite(vib_o, LOW);
-
+    stripm.setBrightness(1);
+    stripm.show();
+    stripr.setBrightness(64); stripr.show();
   }
 
 
-  //环境光
+  //环境光//由于只有两条灯带，此项暂时被禁用
   float _litin = analogRead(lit_i);
-  int lit = abs(  (_litin - 700) * 0.25 );//亮度输出
-  analogWrite(lit_o, lit);
-  //stripl.setBrightness(lit);stripl.show();
+  //Serial.println(_litin);
+  int lit = abs(  (_litin - 500) * 0.25 );//亮度输出
+  //analogWrite(lit_o, lit);
+  //stripr.setBrightness(lit); stripr.show();
 
 
   //声音
-  //    int _sndin = analogRead(snd_ia);
-  //    //int _sndin = digitalRead(snd_id);
-  //    Serial.println(_sndin);
-  //    if (_sndin > 100) {
-  //      digitalWrite(snd_o, HIGH);
-  //    }
-  //    else {
-  //      digitalWrite(snd_o, LOW);
-  //    }
-  //    delay(100);
-
+      int _sndin = analogRead(snd_ia);
+      //int _sndin = digitalRead(snd_id);
+      Serial.println(_sndin);
+      if (_sndin > 390) {
+        digitalWrite(snd_o, HIGH);
+        stripr.setBrightness(255); stripr.show();
+      }
+      else {
+        digitalWrite(snd_o, LOW);
+      }
+    //  delay(100);
   //识别
   if (blt.available() > 0) {
-    int a = blt.read();
-    Serial.println(a);
-    blt.println("data received");
+    unsigned int a = blt.read();
+    //Serial.println(a);
+    //blt.println("data received");
+    //    switch (a) {
+    //      case 4:
+    //        break;
+    //      case 3:
+    //        break;
+    //      case 2:
+    //        break;
+    //      case 1:
+    //        break;
+    //      case 0:
+    //        break;
+    //    }
   }
-  stripl.setBrightness(100); stripl.show();
-  stripm.setBrightness(200); stripm.show();
-  stripr.setBrightness(255); stripr.show();
+  //4~255,3~128,2^64,1~32,0~0
+  //a[0]*(255/4)
+  //stripl.setBrightness(0); stripm.setBrightness(200);  stripr.setBrightness(255);
+  //stripl.show(); stripm.show(); stripr.show();
 }
